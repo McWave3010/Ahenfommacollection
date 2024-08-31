5cf07c63a3f52dc0571df37e771fd2bfff57c949
Samuel Amoh <samuelamoh2005@gmail.com>
2024-08-31 19:54:42 +0000

    collection

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,4 +3,36 @@ document.addEventListener("DOMContentLoaded",()=> {
     whatspp.addEventListener("click",()=>{
         window.location.href = "https://wa.link/z6u1gn";
     })
+    let items = [];
+    async function getData(){
+        await fetch("./assets/json/product.json")
+        .then(response => response.json())
+        .then(data=> {
+             items = data;
+             console.log(items);
+             display(items);
+ 
+        })
+    }
+    getData();
+
+    function display(shoes){
+        const shoesContainer = document.querySelector(".wrapper");
+        shoesContainer.innerHTML = ``;
+        shoes.forEach(element => {
+           const elements = document.createElement("div");
+           elements.classList.add("product-image");
+           elements.style.margin = "0%";
+           elements.innerHTML = `
+                    <div class="image-div">
+                        <img src="${element.image}" id="image1">
+                    </div>
+                    <div class="content">
+                        <span id="ahenfos">${element.name}</span>
+                         <span id="ahenfo">${element.price}</span>
+                    </div>
+           `
+            shoesContainer.appendChild(elements);
+        });
+    }
 });
