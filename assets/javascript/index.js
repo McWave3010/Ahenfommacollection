a547e1935e73f3aaac3b707a7d5f2c79928edd34
Samuel Amoh <samuelamoh2005@gmail.com>
2024-08-31 14:48:44 +0000

    collection

---
+++ assets/javascript/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",()=> {
+    const whatspp = document.querySelector("#app");
+    whatspp.addEventListener("click",()=>{
+        window.location.href = "https://wa.link/z6u1gn";
+    })
+});
